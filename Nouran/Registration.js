db849fa1756216f948959e29ccffb76a4f614224
Batool <batool.com>
2022-11-13 13:15:53 +0200

    Update

--- Nouran/Registration.js
+++ Nouran/Registration.js
@@ -1,17 +1,38 @@
 
-// function getname() {
-//     var firstname = document.getElementById("firstname").value;
-//     console.log(firstname);
-//     localStorage.setItem("firstname",firstname);
-// }
+function getFname() {
+    var firstname = document.getElementById("firstname").value;
+    console.log(firstname);
+    localStorage.setItem("firstname", firstname);
+}
+
+function getLname() {
+    var lastname = document.getElementById("lastname").value;
+    console.log(lastname);
+    localStorage.setItem("lastname", lastname);
+}
+
+function getEmail() {
+    var gEmail = document.getElementById("email").value;
+    console.log(gEmail);
+    localStorage.setItem("Email", gEmail);
+}
+
+function getpassword() {
+    var pass = document.getElementById("password").value;
+
+
+    localStorage.setItem("password", pass);
+}
+
+function ConfirmPsss() {
+    var Confirmpass = document.getElementById("confirmpassword").value;
+
+    localStorage.setItem("Confirm-password", Confirmpass);
+}
+
 
 // localStorage.clear();
-var first=document.getElementById("firstname");
-first.onchange=function(){
-localStorage("firsname",first.value);
-console.log(first)
 
-}
 // var lastname = document.getElementById("lastname");
 // var lastname = document.getElementById("lastname", lastname.value);
 
