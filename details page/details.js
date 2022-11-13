671190b0828efbc15d259365982a973d05dba230
rama-shararah <115452637+rama-shararah@users.noreply.github.com>
2022-11-13 21:19:45 +0300

    p

--- details page/details.js
+++ details page/details.js
@@ -1,5 +1,9 @@
 console.log('working');
 
+// level one
+
+
+// question one 
 document.getElementsByClassName("yourAnswer")[0].innerHTML+=" "+localStorage.getItem("Answer0");
 
 if(localStorage.getItem("Answer0")!="scientifically"){
@@ -9,5 +13,231 @@ else{
     document.getElementsByClassName("yourAnswer")[0].style.backgroundColor="#D3FBE8";
 }
 
+
+// question two 
 document.getElementsByClassName("yourAnswer")[1].innerHTML+=" "+localStorage.getItem("Answer1");
 
+if(localStorage.getItem("Answer1")!="before"){
+    document.getElementsByClassName("yourAnswer")[1].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[1].style.backgroundColor="#D3FBE8";
+}
+
+
+// question three 
+document.getElementsByClassName("yourAnswer")[2].innerHTML+=" "+localStorage.getItem("Answer2");
+
+if(localStorage.getItem("Answer2")!="I was at the museum with Nouran."){
+    document.getElementsByClassName("yourAnswer")[2].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[2].style.backgroundColor="#D3FBE8";
+}
+
+
+// question four
+document.getElementsByClassName("yourAnswer")[3].innerHTML+=" "+localStorage.getItem("Answer3");
+
+if(localStorage.getItem("Answer3")!="done"){
+    document.getElementsByClassName("yourAnswer")[3].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[3].style.backgroundColor="#D3FBE8";
+}
+
+
+// question five
+document.getElementsByClassName("yourAnswer")[4].innerHTML+=" "+localStorage.getItem("Answer4");
+
+if(localStorage.getItem("Answer4")!="I am going to my mother's house."){
+    document.getElementsByClassName("yourAnswer")[4].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[4].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// level two
+
+
+// question one
+document.getElementsByClassName("yourAnswer")[5].innerHTML+=" "+localStorage.getItem("Answer5");
+
+if(localStorage.getItem("Answer5")!="down"){
+    document.getElementsByClassName("yourAnswer")[5].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[5].style.backgroundColor="#D3FBE8";
+}
+
+
+// question two 
+document.getElementsByClassName("yourAnswer")[6].innerHTML+=" "+localStorage.getItem("Answer6");
+
+if(localStorage.getItem("Answer6")!="wore"){
+    document.getElementsByClassName("yourAnswer")[6].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[6].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question three
+document.getElementsByClassName("yourAnswer")[7].innerHTML+=" "+localStorage.getItem("Answer7");
+
+if(localStorage.getItem("Answer7")!="We're wearing suits."){
+    document.getElementsByClassName("yourAnswer")[7].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[7].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question four
+document.getElementsByClassName("yourAnswer")[8].innerHTML+=" "+localStorage.getItem("Answer8");
+
+if(localStorage.getItem("Answer8")!="into"){
+    document.getElementsByClassName("yourAnswer")[8].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[8].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question five
+document.getElementsByClassName("yourAnswer")[9].innerHTML+=" "+localStorage.getItem("Answer9");
+
+if(localStorage.getItem("Answer9")!="get along"){
+    document.getElementsByClassName("yourAnswer")[9].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[9].style.backgroundColor="#D3FBE8";
+}
+
+
+
+
+//level three
+
+
+
+// question one
+document.getElementsByClassName("yourAnswer")[10].innerHTML+=" "+localStorage.getItem("Answer10");
+
+if(localStorage.getItem("Answer10")!="got back"){
+    document.getElementsByClassName("yourAnswer")[10].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[10].style.backgroundColor="#D3FBE8";
+}
+
+
+// question two 
+document.getElementsByClassName("yourAnswer")[11].innerHTML+=" "+localStorage.getItem("Answer11");
+
+if(localStorage.getItem("Answer11")!="blew"){
+    document.getElementsByClassName("yourAnswer")[11].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[11].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question three
+document.getElementsByClassName("yourAnswer")[12].innerHTML+=" "+localStorage.getItem("Answer12");
+
+if(localStorage.getItem("Answer12")!="were able to knock"){
+    document.getElementsByClassName("yourAnswer")[12].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[12].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question four
+document.getElementsByClassName("yourAnswer")[13].innerHTML+=" "+localStorage.getItem("Answer13");
+
+if(localStorage.getItem("Answer8")!="nor"){
+    document.getElementsByClassName("yourAnswer")[13].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[13].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question five
+document.getElementsByClassName("yourAnswer")[14].innerHTML+=" "+localStorage.getItem("Answer14");
+
+if(localStorage.getItem("Answer14")!="were given / would pick"){
+    document.getElementsByClassName("yourAnswer")[14].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[14].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question six
+document.getElementsByClassName("yourAnswer")[15].innerHTML+=" "+localStorage.getItem("Answer15");
+
+if(localStorage.getItem("Answer15")!="so / that"){
+    document.getElementsByClassName("yourAnswer")[15].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[15].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question seven
+document.getElementsByClassName("yourAnswer")[16].innerHTML+=" "+localStorage.getItem("Answer16");
+
+if(localStorage.getItem("Answer16")!="Every time / how"){
+    document.getElementsByClassName("yourAnswer")[16].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[16].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question eight
+document.getElementsByClassName("yourAnswer")[17].innerHTML+=" "+localStorage.getItem("Answer17");
+
+if(localStorage.getItem("Answer17")!="why"){
+    document.getElementsByClassName("yourAnswer")[17].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[17].style.backgroundColor="#D3FBE8";
+}
+
+
+// question nine
+document.getElementsByClassName("yourAnswer")[18].innerHTML+=" "+localStorage.getItem("Answer18");
+
+if(localStorage.getItem("Answer18")!="originated / began"){
+    document.getElementsByClassName("yourAnswer")[18].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[18].style.backgroundColor="#D3FBE8";
+}
+
+
+
+// question ten
+document.getElementsByClassName("yourAnswer")[19].innerHTML+=" "+localStorage.getItem("Answer19");
+
+if(localStorage.getItem("Answer19")!="had been lying / was spotted"){
+    document.getElementsByClassName("yourAnswer")[19].style.backgroundColor="#FFD2D3";
+}
+else{
+    document.getElementsByClassName("yourAnswer")[19].style.backgroundColor="#D3FBE8";
+}
