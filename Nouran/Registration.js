612b39503cb99e1f2fd09f1c77c324821710ee9b
Batool <batool.com>
2022-11-19 22:26:21 +0300

    project3

--- Nouran/Registration.js
+++ Nouran/Registration.js
@@ -1,101 +1,57 @@
-var i = 0;
+
+
 // localStorage.clear();
+var i = 0;
+var flag = true;
 function signup() {
-    var flag = true;
-    localStorage.setItem("i", i);
 
-    //take rom input box (sign up)
     var firstname = document.getElementById("firstname").value;
-
-
-
-    for (var x = 0; x < 10; x++) {
+    for (let x = 0; x < 10; x++) {
         if (firstname == localStorage.getItem("firstname" + x)) {
-            alert("username is already taken");
+            alert("you are aleady exit");
             flag = false;
             break;
-
-
         }
-
-
-
-
-
     }
     if (flag) {
-        i++;
-        //set to local storage
         localStorage.setItem("firstname" + i, firstname);
-        // console.log(typeof "result");
-        // if (result == 0) {
-        //     location.href = 'project3.html';
-        // }
-        // console.log(localStorage.getItem("result" + userrepeat));
+
+
         var lastname = document.getElementById("lastname").value;
-        // console.log(lastname);
         localStorage.setItem("lastname" + i, lastname);
-        var gEmail = document.getElementById("email").value;
-        // console.log(gEmail);
-        localStorage.setItem("Email" + i, gEmail);
-        var pass = document.getElementById("password").value;
-        localStorage.setItem("password" + i, pass);
-        var Confirmpass = document.getElementById("confirmpassword").value;
-        localStorage.setItem("Confirm-password" + i, Confirmpass);
+        var email = document.getElementById("email").value;
+        localStorage.setItem("email" + i, email);
 
+        var password = document.getElementById("password").value;
+        localStorage.setItem("password" + i, password);
 
-
-        location.href = "../Batool/Sign-in.html"
-
-
-
-
-
-
-
-
-        // var x = document.getElementById("confirmpassword");
-        // // console.log(x);
-
-        // localStorage.setItem("batool123", x);
-
-        // console.log(localStorage.getItem("batool123"));
+        var confirmpassword = document.getElementById("confirmpassword").value;
+        console.log(confirmpassword);
+        localStorage.setItem("confirmpassword" + i, confirmpassword);
+        i++
     }
-}
-// localStorage.clear();
 
-// var lastname = document.getElementById("lastname");
-// var lastname = document.getElementById("lastname", lastname.value);
-
-function ValidateEmail(p) {
-    var mailformat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-    var email = p.value;
-    console.log(email);
-    if (!(email.match(mailformat))) {
-        alert("This is not a valid email address");
-        return false;
-    }
 }
 
 
-function ValidatePassword(p) {
-    var passFormat = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-    var pass = p.value;
-    console.log(pass);
-    if (!(pass.match(passFormat))) {
-        alert("This is not a valid Password");
-        return false;
-    }
-}
 
-// var pass = document.getElementById("password").value;
-// console.log
-// var Confirmpass = document.getElementById("confirmpassword").value;
-// function equalpass() {
-//     if (pass != Confirmpass) {
-//         alert("Wrong pass");
+// function ValidateEmail(p) {
+//     var mailformat = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+//     var email = p.value;
+//     console.log(email);
+//     if (!(email.match(mailformat))) {
+//         alert("This is not a valid email address");
+//         return false;
 //     }
-//     else {
-//         alert("works")
+// }
+
+
+// function ValidatePassword(p) {
+//     var passFormat = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
+//     var pass = p.value;
+//     console.log(pass);
+//     if (!(pass.match(passFormat))) {
+//         alert("This is not a valid Password");
+//         return false;
 //     }
 // }
